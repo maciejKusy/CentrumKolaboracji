9ce1e9cd34ba1fcc9434bc96599f4343f5610d05
MKurtz666 <mkusy936@gmail.com>
2021-02-12 19:23:49 +0100

    pierwsza sesja done

--- borotask/src/model.js
+++ borotask/src/model.js
@@ -1,5 +1,45 @@
-class Model {
+import {Sign, Status} from './consts.js';
 
+class Model {
+    getSigns = () => {
+        return [Sign.ROCK, Sign.SCISSORS, Sign.PAPER];
+    }
+
+    descSign = sign => {
+        switch (sign) {
+            case Sign.ROCK: {
+                return {
+                    [Sign.SCISSORS]: Status.WIN,
+                    [Sign.PAPER]: Status.LOSE
+                }
+            }
+            case Sign.PAPER: {
+                return {
+                    [Sign.ROCK]: Status.WIN,
+                    [Sign.SCISSORS]: Status.LOSE,
+                }
+            }
+            case Sign.SCISSORS: {
+                return {
+                    [Sign.PAPER]: Status.WIN,
+                    [Sign.ROCK]: Status.LOSE,
+                }
+            }
+        }
+    }
+    decide = (sign1, sign2) => {
+        const signContent = this.descSign(sign1);
+        
+        if (sign1 === sign2) {return Status.DRAW};
+
+        return signContent[sign2];
+    }
+    random = () => {
+        const index = Math.floor(Math.random() * 3);
+
+        const choices = this.getSigns();
+        return choices[index];
+    }
 }
 
 export default Model;
--- borotask/test/model.spec.js
+++ borotask/test/model.spec.js
@@ -13,66 +13,65 @@ describe("model", () => {
         expect(sut).to.be.an('object')
     })
 
-    // it("getSigns should return all available Signs", () => {
-    //     const result = sut.getSigns();
+    it("getSigns should return all available Signs", () => {
+        const result = sut.getSigns();
 
-    //     expect(result).to.deep.equal([Sign.ROCK, Sign.SCISSORS, Sign.PAPER])
-    // })
+        expect(result).to.deep.equal([Sign.ROCK, Sign.SCISSORS, Sign.PAPER])
+    })
 
-    // describe("descSign", ()=> {
-    //     [
-    //         {value: Sign.ROCK, expected: {
-    //             [Sign.SCISSORS]: Status.WIN,
-    //             [Sign.PAPER]: Status.LOSE
-    //         }}, 
-    //         {value: Sign.PAPER, expected: {
-    //             [Sign.ROCK]: Status.WIN,
-    //             [Sign.SCISSORS]: Status.LOSE,
-    //         }},
-    //         {value: Sign.SCISSORS, expected: {
-    //             [Sign.PAPER]: Status.WIN,
-    //             [Sign.ROCK]: Status.LOSE,
-    //         }}
-    //     ].forEach((testCase) => {
-    //         it("should return desc of " + testCase.value, ()=> {
-    //             const result = sut.descSign(testCase.value);
+    describe("descSign", ()=> {
+        [
+            {value: Sign.ROCK, expected: {
+                [Sign.SCISSORS]: Status.WIN,
+                [Sign.PAPER]: Status.LOSE
+            }}, 
+            {value: Sign.PAPER, expected: {
+                [Sign.ROCK]: Status.WIN,
+                [Sign.SCISSORS]: Status.LOSE,
+            }},
+            {value: Sign.SCISSORS, expected: {
+                [Sign.PAPER]: Status.WIN,
+                [Sign.ROCK]: Status.LOSE,
+            }}
+        ].forEach((testCase) => {
+            it("should return desc of " + testCase.value, ()=> {
+                const result = sut.descSign(testCase.value);
         
-    //             expect(result).to.deep.equal(testCase.expected)
-    //         })
-    //     });
-    // })
+                expect(result).to.deep.equal(testCase.expected)
+            })
+        });
+    })
     
-    // describe("decide", ()=> {
-    //     [
-    //         {input: [Sign.ROCK, Sign.ROCK], expected: Status.DRAW},
-    //         {input: [Sign.ROCK, Sign.PAPER], expected: Status.LOSE},
-    //         {input: [Sign.ROCK, Sign.SCISSORS], expected: Status.WIN},
-    //         {input: [Sign.PAPER, Sign.PAPER], expected: Status.DRAW},
-    //         {input: [Sign.PAPER, Sign.SCISSORS], expected: Status.LOSE},
-    //         {input: [Sign.PAPER, Sign.ROCK], expected: Status.WIN},
-    //         {input: [Sign.SCISSORS, Sign.SCISSORS], expected: Status.DRAW},
-    //         {input: [Sign.SCISSORS, Sign.ROCK], expected: Status.LOSE},
-    //         {input: [Sign.SCISSORS, Sign.PAPER], expected: Status.WIN},
-    //     ].forEach((testCase) => {
-    //         it(`should return ${testCase.expected} for ${testCase.input[0]} over ${testCase.input[1]} `, ()=> {
-    //             const result = sut.decide(...testCase.input);
+    describe("decide", ()=> {
+        [
+            {input: [Sign.ROCK, Sign.ROCK], expected: Status.DRAW},
+            {input: [Sign.ROCK, Sign.PAPER], expected: Status.LOSE},
+            {input: [Sign.ROCK, Sign.SCISSORS], expected: Status.WIN},
+            {input: [Sign.PAPER, Sign.PAPER], expected: Status.DRAW},
+            {input: [Sign.PAPER, Sign.SCISSORS], expected: Status.LOSE},
+            {input: [Sign.PAPER, Sign.ROCK], expected: Status.WIN},
+            {input: [Sign.SCISSORS, Sign.SCISSORS], expected: Status.DRAW},
+            {input: [Sign.SCISSORS, Sign.ROCK], expected: Status.LOSE},
+            {input: [Sign.SCISSORS, Sign.PAPER], expected: Status.WIN},
+        ].forEach((testCase) => {
+            it(`should return ${testCase.expected} for ${testCase.input[0]} over ${testCase.input[1]} `, ()=> {
+                const result = sut.decide(...testCase.input);
         
-    //             expect(result).to.deep.equal(testCase.expected)
-    //         })
-    //     });
-    // })
+                expect(result).to.deep.equal(testCase.expected)
+            })
+        });
+    })
 
-    // it('random returns random sign', () => {
-    //     const count = {}
-    //     for(let i = 0; i < 100; i++) {
-    //         const sign = sut.random();
-    //         count[sign] = count[sign] ? count[sign] + 1 : 1
-    //     }
+    it('random returns random sign', () => {
+        const count = {}
+        for(let i = 0; i < 100; i++) {
+            const sign = sut.random();
+            count[sign] = count[sign] ? count[sign] + 1 : 1
+        }
 
-    //     expect(count[Sign.PAPER]).to.be.above(20)
-    //     expect(count[Sign.ROCK]).to.be.above(20)
+        expect(count[Sign.PAPER]).to.be.above(20)
+        expect(count[Sign.ROCK]).to.be.above(20)
     
-    //     expect(count[Sign.SCISSORS]).to.be.above(20)
-    // })
-
+        expect(count[Sign.SCISSORS]).to.be.above(20)
+    })
 })
